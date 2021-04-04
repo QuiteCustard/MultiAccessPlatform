6b485c699109266cb235e2990122112a74cbedb6
SamEdwards141 <40074746+SamEdwards141@users.noreply.github.com>
2021-04-04 02:54:22 +0100

    Hopeful fix for random user adding

--- admin/cases.php
+++ admin/cases.php
@@ -312,25 +312,30 @@ if (isset($_GET['case']))
             case "insertUser":
                 if (isset($_POST['email'])){
                     $email = mysqli_real_escape_string($db_connect, $_POST['email']);
-                    if (isset($email)){
-                        $id = rand();
-                        $password = $_POST['password'];
-                        $fname = $_POST['fname'];
-                        $lname = $_POST['lname'];
-                        $job = $_POST['job'];
-                        $access = $_POST['access'];
-                        $course = $_POST['course'];
-                        $cid = $_POST['newUserCid'];
-                        // Insert new record
-                        $sqlQuery = "INSERT INTO `t_users` (`UID`, `Fname`, `Lname`, `Jobtitle`, `Email`, `Password`, `Access`, `Currentcourse`, `Attempts`, `Timestamp`, `Serial`) VALUES ('$id', '$fname', '$lname', '$job', '$email', '$password', '$access', '$course', '0', current_timestamp(), NULL);";
-                        mysqli_query($db_connect, $sqlQuery);
-                        echo "Record INSERTED successfully";
-                        // Function to insert a new record to enrolment table
-                        function insertEnrolmentTable($db_connect, $id, $cid){
-                            $sql = "INSERT INTO t_enrolment (UID, CID) VALUES ($id, $cid);";
-                            mysqli_query($db_connect, $sql);
+                    if ($email == ""){
+                        die("your email is empty somehow");
+                    }
+                    else{
+                        if (isset($email)){
+                            $id = rand();
+                            $password = $_POST['password'];
+                            $fname = $_POST['fname'];
+                            $lname = $_POST['lname'];
+                            $job = $_POST['job'];
+                            $access = $_POST['access'];
+                            $course = $_POST['course'];
+                            $cid = $_POST['newUserCid'];
+                            // Insert new record
+                            $sqlQuery = "INSERT INTO `t_users` (`UID`, `Fname`, `Lname`, `Jobtitle`, `Email`, `Password`, `Access`, `Currentcourse`, `Attempts`, `Timestamp`, `Serial`) VALUES ('$id', '$fname', '$lname', '$job', '$email', '$password', '$access', '$course', '0', current_timestamp(), NULL);";
+                            mysqli_query($db_connect, $sqlQuery);
+                            echo "Record INSERTED successfully";
+                            // Function to insert a new record to enrolment table
+                            function insertEnrolmentTable($db_connect, $id, $cid){
+                                $sql = "INSERT INTO t_enrolment (UID, CID) VALUES ($id, $cid);";
+                                mysqli_query($db_connect, $sql);
+                            }
+                            insertEnrolmentTable($db_connect, $id, $cid);
                         }
-                        insertEnrolmentTable($db_connect, $id, $cid);
                     }
                 }else{
                     echo "Failed to update record: No ID!";
