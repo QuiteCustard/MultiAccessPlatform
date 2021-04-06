6db628a897ba76e736f85c80203bc87160b24312
SamEdwards141 <40074746+SamEdwards141@users.noreply.github.com>
2021-04-06 23:53:04 +0100

    Form style

--- newPassword.php
+++ newPassword.php
@@ -25,9 +25,30 @@ if(isset($_GET["token"]))
         // Only include styles if user has token
         include_once("/include/head.php");
 ?>
+<body class="tertiary">
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 primary text">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 mb-4 textOuter">Reset your password here!</h1>
+                                    </div>
+                                    <form method="POST" action="#">
+                                        <div class="form-group">
+                                            <input name="newPassword" type="password" required placeholder="new password">
+                                        </div>
+                                        <button class="btn btn-primary btn-user btn-block secondary secondaryBorder" required placeholder="new password" action="submit" type="submit">Update Password</button>
+
 <form method="POST" action="#">
     <input name="newPassword" type="password" required placeholder="new password">
-    <button class="btn btn-primary" type="submit">Update Password</button>
+    <button class="btn btn-primary" type="submit"></button>
 </form>
 <?php
     }
