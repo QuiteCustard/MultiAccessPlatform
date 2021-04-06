18de5f1323fa4b4b666f3911ab61de687b2b7f4f
SamEdwards141 <40074746+SamEdwards141@users.noreply.github.com>
2021-04-07 00:03:55 +0100

    Styles further

--- newPassword.php
+++ newPassword.php
@@ -25,29 +25,37 @@ if(isset($_GET["token"]))
     {
         // Only include if user has token
 ?>
+
 <body class="tertiary">
     <div class="container">
+
         <!-- Outer Row -->
         <div class="row justify-content-center">
+
             <div class="col-xl-10 col-lg-12 col-md-9">
+
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6 primary text">
+                            <div class="col-lg-6 primary">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 mb-4 textOuter">Reset your password here!</h1>
+                                        <h1 class="h4 textOuter mb-2">Reset your password here!</h1>
+                                        <p class="mb-4">We get it, stuff happens and you forgot your password, lucky you can reset it here, right?</p>
                                     </div>
                                     <form method="POST" action="#">
                                         <div class="form-group">
-                                            <input name="newPassword" type="password" required placeholder="new password">
+                                            <input name="newPassword" type="password" class="form-control form-control-user primary" required placeholder="new password">
                                         </div>
                                         <button class="btn btn-primary btn-user btn-block secondary secondaryBorder" required placeholder="new password" action="submit" type="submit">Update Password</button>
                                     </form>
 
-<?php
+
+
+
+                                    <?php
     }
 }
 else
