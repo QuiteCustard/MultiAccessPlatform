a06b70ba37a56cc3be330fa4dc848e02cb4a884b
SamEdwards141 <40074746+SamEdwards141@users.noreply.github.com>
2021-04-07 20:43:29 +0100

    Prepared statement for login

--- admin/auth.php
+++ admin/auth.php
@@ -1,75 +1,86 @@
 <?php
-
-if( !isset($_POST["Email"]) or !isset($_POST["Password"]))
-{
+if(!(isset($_POST["Email"]) && !isset($_POST["Password"]))){
 //if no values are passed from login form, return to previous page (login)
 header("Location: ../index.php?e=1");
 }
 require_once("connect.php");
 //if values are found, proceed:
-$email = mysqli_escape_string($mysqli, $_POST["Email"]);
+$email = mysqli_real_escape_string($mysqli, $_POST["Email"]);
 //convert plain password to a hash
-$password = mysqli_escape_string($mysqli,hash("SHA256",$_POST["Password"]));
-
-$currentAttempts = mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT `Email`,`Attempts` FROM `t_users` WHERE `Email` = '$email'"))['Attempts'];
-
-$newAttempts = $currentAttempts + 1;
-mysqli_query($mysqli, "UPDATE `t_users` SET `Attempts` = '$newAttempts' WHERE `email` = '$email';");
-
-if($currentAttempts > 4)
-{
-
-    //Recapta
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
-
-    // Build POST request:
-    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-    $recaptcha_secret = '6Le7Tm4aAAAAAHXN67gA7d9ajGAUdH_g0iW_K7z0';
-    $recaptcha_response = $_POST['recaptcha_response'];
-
-    // Make and decode POST request:
-    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
-    $recaptcha = json_decode($recaptcha);
-
-    // Take action based on the score returned:
-    if ($recaptcha->score >= 0.5) {
-        // Verified - send email
-    } else {
-        // Not verified - show form error
+$password = mysqli_real_escape_string($mysqli,hash("SHA256",$_POST["Password"]));
+// Get current attempts
+// Prepared statement
+$stmt = $mysqli->prepare("SELECT Email, Attempts FROM `t_users` WHERE Email = ?");
+// Put parameter into query
+$stmt->bind_param("s", $email);
+// Run the statement
+$stmt->execute();
+// Set result to this query
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $currentAttempts = $row['Attempts'];
     }
 }
+// Set new attempts to +1 of old value
+$newAttempts = $currentAttempts + 1;
+// Prepared statement
+$stmt = $mysqli->prepare("UPDATE `t_users` SET `Attempts` = ? WHERE `Email` = ?");
+// Put parameters into query
+$stmt->bind_param("is", $newAttempts, $email);
+// Run statement
+$stmt->execute();
+// Close statement
+$stmt->close();
+if($currentAttempts > 4){
+    //Recapta
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+        // Build POST request:
+        $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+        $recaptcha_secret = '6Le7Tm4aAAAAAHXN67gA7d9ajGAUdH_g0iW_K7z0';
+        $recaptcha_response = $_POST['recaptcha_response'];
+        // Make and decode POST request:
+        $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+        $recaptcha = json_decode($recaptcha);
+        // Take action based on the score returned:
+        if ($recaptcha->score >= 0.5) {
+            // Verified - send email
+        }else{
+            // Not verified - show form error
+        }
+    }
 }
-//run query
-$query = "SELECT * FROM `t_users` WHERE `Email` = '$email' AND `Password` = '$password'";
-
-$run = mysqli_query($mysqli,$query);
-//count matches.  Will return 1 if match found.
-$result =  mysqli_fetch_assoc($run);
-$count = mysqli_num_rows($run);
-
-//will return 1 if a username/password match is found
-if ($count === 1){
-    // Check access level
-    $access = $result["Access"];
-    $userid = $result["UID"];
-  // let us log in
-    session_start();
-    $_SESSION["auth"] = $access;
-    // Reset login attempts
-    $resetAttempts = 0;
-    $sqlQuery="UPDATE `t_users` SET `Attempts` = '0' WHERE `t_users`.`UID` = '$userid';";
-    mysqli_query($mysqli,$sqlQuery);
-    echo "Record updated successfully";
-    // Remember name
-    $_SESSION["name"] = $result["Fname"] . " " . $result["Lname"];
-    $_SESSION["userid"] = $userid;
-    echo json_encode($userid);
-    header("Location: index.php");
-    die();
-}
-else 
-{
-// if 1 is not returned, redirect to login page with e=2
-   header("Location: ../index.php?e=2");
+// Prepared statement
+$stmt = $mysqli->prepare("SELECT * FROM `t_users` WHERE Email = ? AND Password = ?");
+// Put parameter into query
+$stmt->bind_param("ss", $email, $password);
+// Run the statement
+$stmt->execute();
+// Set result to this query
+$result = $stmt->get_result();
+if ($result->num_rows === 1) {
+    while ($row = $result->fetch_assoc()) {
+         // Check access level
+        $access = $row["Access"];
+        $userid = $row["UID"];
+        // let us log in
+        session_start();
+        $_SESSION["auth"] = $access;
+        // Reset login attempts
+        $resetAttempts = 0;
+        $sqlQuery="UPDATE `t_users` SET `Attempts` = '0' WHERE `t_users`.`UID` = '$userid';";
+        mysqli_query($mysqli,$sqlQuery);
+        echo "Record updated successfully";
+        // Remember name
+        $_SESSION["name"] = $row["Fname"] . " " . $row["Lname"];
+        $_SESSION["userid"] = $userid;
+        echo json_encode($userid);
+        header("Location: index.php");
+        die("Log in successful");
+    }
+}else {
+    // if 1 is not returned, redirect to login page with e=2
+    header("Location: ../index.php?e=2");
     die("Wrong information");
 }
+
