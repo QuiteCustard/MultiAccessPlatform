b658a3bc07528dcf4aeda942067cf7fea2d3726c
SamEdwards141 <40074746+SamEdwards141@users.noreply.github.com>
2021-04-08 01:38:34 +0100

    Search remove

--- admin/userOnCourse.php
+++ admin/userOnCourse.php
@@ -7,10 +7,6 @@ require_once("_logincheck.php");
     <div class='col-md-10'>
         <h3>Courses</h3>
     </div>
-    <div class='col-md-2'>
-        <input type="text" id="search" class="form-control primary inputs" placeholder="Search.." />
-        <p class="text-center">You can search by all rows</p>
-    </div>
 </div>
 <div class='table-responsive tUserCourse'></div>
 <div class="response"></div>
@@ -31,42 +27,6 @@ require_once("_logincheck.php");
             });
         }
         var intervalTiming = setInterval(getEnrolCourseData(), 60000);
-        // User search
-        function userSearch() {
-            // Update data after every key press
-            $('#search').on('keyup', function() {
-                const input = $("#search");
-                const filter = input.val().toUpperCase();
-                const table = $(".removeUserBody");
-                const tr = table.find("tr");
-                for (i = 0; i < tr.length; i++) {
-                    // Find all tds
-                    tds = $(tr[i]).find("td");
-                    // Set found to false so I can update styles later if results are found/not
-                    var found = false;
-                    // Set all tds to be searchable
-                    for (j = 0; j < tds.length; j++) {
-                        td = tds[j];
-                        if (td) {
-                            if (td.innerText.toUpperCase().indexOf(filter) > -1) {
-                                found = true;
-                                break;
-                            }
-                        }
-                    }
-                    // Styles
-                    if (found) {
-                        tr[i].style.display = "";
-                        $(tr[i]).addClass("tertiarySelect");
-
-                    } else {
-                        tr[i].style.display = "none";
-                        $(tr[i]).removeClass("tertiarySelect");
-                    }
-                }
-            });
-        };
-        userSearch();
         // Remove user from course
         $('body').off('click', '.remove').on('click', '.remove', function(e) {
             const removeButton = $(e.target);
